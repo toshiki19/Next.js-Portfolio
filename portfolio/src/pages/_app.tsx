aafce864887dc177e7b62f5bcbbd2db305a0e7af
toshiki19 <toshiki.u.19@gmail.com>
2023-06-08 18:43:28 +0900

    ページの作成

--- portfolio/src/pages/_app.tsx
+++ portfolio/src/pages/_app.tsx
@@ -1,6 +1,26 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import NavBar from '@/components/navbar'
+import Footer from '@/components/footer'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={'${inter.variable} font-sans bg-light w-full min-h-screen'}>
+        <NavBar />
+        <Component {...pageProps} />
+        <Footer />
+      </main>
+  </>
+  );
 }
